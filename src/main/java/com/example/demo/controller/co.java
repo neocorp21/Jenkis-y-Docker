5a9984569f6aa70b57bea9adcafd2f22b18ca0bf
Alberto <alberto.limas.contractor@bbva.com>
2024-11-07 23:30:02 -0500

    feat

--- src/main/java/com/example/demo/controller/co.java
+++ src/main/java/com/example/demo/controller/co.java
@@ -8,6 +8,6 @@ public class co {
 
         @GetMapping("/hola")
         public String hola() {
-            return "Hola Mundo  desde Spring Boot Ded";
+            return "Hola Mundo  desde Spring Boot Ded 2";
         }
 }
